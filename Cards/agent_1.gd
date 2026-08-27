3311fddba1e436fa30b5832c882b2bf296e13e0d
gugagzobava <ggzob23@freeuni.edu.ge>
2026-08-27 15:20:48 +0400

    added some characters

--- Cards/agent_1.gd
+++ Cards/agent_1.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
+@export var picture: Texture2D
+
 var good = true
 signal clicked(num: int)
 var id = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	$Sprite2D.texture = picture
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
